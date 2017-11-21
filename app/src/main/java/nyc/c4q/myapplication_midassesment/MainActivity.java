15d7472e40ebeb43ec37b42b01051022b276859b
bRad-95 <bryantramirez@ac.c4q.nyc>
2017-11-20 19:59:06 -0500

    second commit

--- app/src/main/java/nyc/c4q/myapplication_midassesment/MainActivity.java
+++ app/src/main/java/nyc/c4q/myapplication_midassesment/MainActivity.java
@@ -1,7 +1,11 @@
 package nyc.c4q.myapplication_midassesment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import nyc.c4q.myapplication_midassesment.Fragments.ButtonFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButtonFragment mbuttonFragment = new ButtonFragment();
+        FragmentManager mfragmentManager = getSupportFragmentManager();
+        FragmentTransaction mfragmentTransaction = mfragmentManager.beginTransaction();
+        mfragmentTransaction.replace(R.id.main_container, mbuttonFragment);
+        mfragmentTransaction.commit();
     }
 }
